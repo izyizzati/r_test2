b7a8daf40f39d3411e1874a11d70ff92aab525d5
izyizzati <izzatiani@gmail.com>
2024-05-09 14:57:13 +0100

    Add files via upload

---
+++ assignment.R
@@ -0,0 +1,6 @@
+summary(DomesticBeer)
+plot(DomesticBeer)
+plot(DomesticBeer$Alcohol,DomesticBeer$Carbs)
+
+
+For reference, the Institute of Medicine proposes Americans obtain 45%-65% of calories from carbohydrates.[1]  This article will review the evidence and effectiveness of low-carb approaches in clinical medicine.
